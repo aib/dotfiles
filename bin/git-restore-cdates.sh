884f808c4dff952fedecfdf28342b1db76948f5e
aib <aibok42@gmail.com>
2017-05-10 05:59:15 +0300

    Allow an extra -f to be passed to git-restore-cdates

--- bin/git-restore-cdates.sh
+++ bin/git-restore-cdates.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git filter-branch --env-filter "export GIT_COMMITTER_DATE=\$(grep -F -m 1 \"\$(git log -n 1 --format=\"%aI %s\" \$GIT_COMMIT)\" $1 | cut -d\" \" -f2)"
+git filter-branch --env-filter "export GIT_COMMITTER_DATE=\$(grep -F -m 1 \"\$(git log -n 1 --format=\"%aI %s\" \$GIT_COMMIT)\" $1 | cut -d\" \" -f2)" $2
