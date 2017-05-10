e45ffd132d09cbe8622b5b297593cb1973c03890
aib <aibok42@gmail.com>
2017-05-10 06:09:56 +0300

    Update makelinks.sh to create $HOME/bin/ links as well

--- makelinks.sh
+++ makelinks.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+BINDIR="${HOME}/bin"
+
 ln -t "${HOME}" -s -r .gdbinit .gitconfig .inputrc .vim .vimrc .zshrc
+
+if [[ ! -e "${BINDIR}" ]]; then
+	mkdir "${BINDIR}"
+fi
+
+if [[ -d "${BINDIR}" ]]; then
+	ln -t "${BINDIR}" -s -r bin/*
+fi
