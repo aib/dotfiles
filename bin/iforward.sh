b623a04a4b8f43d54ba8f139aa5f9280841f7214
aib <aibok42@gmail.com>
2017-06-21 16:43:34 +0300

    Add quoting and style changes to iforward.sh

--- bin/iforward.sh
+++ bin/iforward.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ -z "$1" -o -z "$2" ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
 	printf "Usage:\n\t%s <LAN-if> <WAN-if>\n\n" "$0" >&2
 	exit 1
 fi
 
+LANIF="$1"
+WANIF="$2"
+
 sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -o $2 -j MASQUERADE
-iptables -A FORWARD -i $2 -o $1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i $1 -o $2 -j ACCEPT
+iptables -t nat -A POSTROUTING -o "$WANIF" -j MASQUERADE
+iptables -A FORWARD -i "$WANIF" -o "$LANIF" -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i "$LANIF" -o "$WANIF" -j ACCEPT
