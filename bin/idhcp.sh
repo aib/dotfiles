5adc052abadd5d38486cd3203766e049e6bc08bf
aib <aibok42@gmail.com>
2017-07-04 02:00:53 +0300

    Add quoting changes to idhcp.sh

--- bin/idhcp.sh
+++ bin/idhcp.sh
@@ -9,13 +9,13 @@ fi
 
 LANIF="$1"
 
-carrier_file=/sys/class/net/"${LANIF}"/carrier
+carrier_file=/sys/class/net/"$LANIF"/carrier
 if [ ! -r "$carrier_file" ] || [ $(<"$carrier_file") -ne "1" ]; then
-	printf "Waiting for %s link up..." "${LANIF}"
+	printf "Waiting for %s link up..." "$LANIF"
 	while [ ! -r "$carrier_file" ] || [ $(<"$carrier_file") -ne "1" ]; do
 		sleep 0.01
 	done
 fi
 
-ip address add 192.168.42.1/24 dev ${LANIF}
-dnsmasq -d -p 0 -F 192.168.42.2,192.168.42.2,4h -O 3,192.168.42.1 -O 6,${DNSSERV} --dhcp-leasefile=/dev/null
+ip address add 192.168.42.1/24 dev "$LANIF"
+dnsmasq -d -p 0 -F 192.168.42.2,192.168.42.2,4h -O 3,192.168.42.1 -O 6,"$DNSSERV" --dhcp-leasefile=/dev/null
