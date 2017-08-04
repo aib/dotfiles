557d194115398644a26ee29db3c4449ad142a708
aib <aibok42@gmail.com>
2017-08-04 05:33:22 +0300

    Let git-restore-* accept arbitrary numbers of filter-branch parameters

--- bin/git-restore-both-dates.sh
+++ bin/git-restore-both-dates.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-git filter-branch --env-filter "export GIT_COMMITTER_DATE=\$(grep -F -m 1 \"\$(git log -n 1 --format=\"%aI %s\" \$GIT_COMMIT)\" $1 | cut -d\" \" -f2); export GIT_AUTHOR_DATE=\$(grep -F -m 1 \"\$(git log -n 1 --format=\"%aI %s\" \$GIT_COMMIT)\" $1 | cut -d\" \" -f2)" $2
+backup_file="$1"
+shift
+
+git filter-branch --env-filter "export GIT_COMMITTER_DATE=\$(grep -F -m 1 \"\$(git log -n 1 --format=\"%aI %s\" \$GIT_COMMIT)\" "$backup_file" | cut -d\" \" -f2); export GIT_AUTHOR_DATE=\$(grep -F -m 1 \"\$(git log -n 1 --format=\"%aI %s\" \$GIT_COMMIT)\" "$backup_file" | cut -d\" \" -f2)" "$@"
