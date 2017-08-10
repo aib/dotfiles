d75398270c2824a02f424f9f99dea7fd53ae5b96
aib <aibok42@gmail.com>
2017-08-10 23:57:14 +0300

    Add .screenrc to makelinks.sh

--- makelinks.sh
+++ makelinks.sh
@@ -8,7 +8,7 @@ makelink() {
 	ln -t "${target_dir}" -s -r "$@"
 }
 
-makelink "${HOME}" .gdbinit .gitconfig .inputrc .vim .vimrc .zshrc
+makelink "${HOME}" .gdbinit .gitconfig .inputrc .screenrc .vim .vimrc .zshrc
 
 if [[ ! -e "${BINDIR}" ]]; then
 	mkdir "${BINDIR}"
