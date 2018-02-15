4f94e0c929c199303f94165607267d5a5420f379
aib <aibok42@gmail.com>
2018-02-15 14:48:11 +0300

    Add Xfce Terminal keyboard shortcuts

---
+++ .config/xfce4/terminal/accels.scm
@@ -0,0 +1,4 @@
+(gtk_accel_path "<Actions>/terminal-window/move-tab-left" "<Primary><Shift>Left")
+(gtk_accel_path "<Actions>/terminal-window/move-tab-right" "<Primary><Shift>Right")
+(gtk_accel_path "<Actions>/terminal-window/next-tab" "<Shift>Right")
+(gtk_accel_path "<Actions>/terminal-window/prev-tab" "<Shift>Left")
