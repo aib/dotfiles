44d005c08ac6680fe1a24d9d1735f0e19b35d46b
aib <aibok42@gmail.com>
2018-02-15 14:49:15 +0300

    Autolink .config

--- makelinks.sh
+++ makelinks.sh
@@ -26,4 +26,4 @@ autolink_dir() {
 
 autolink .aliases .bash_aliases .environment .gdbinit .gitconfig .inputrc .screenrc .vimrc .XCompose .zshrc
 
-autolink_dir bin .local .ssh .vim
+autolink_dir bin .config .local .ssh .vim
